114f6226411e43781c336897e06bf3019455f88b
Thomas Cianfarani <thomas.ci83@gmail.com>
2020-03-20 19:12:05 +0100

    Handle locked/unlocked commanders display

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -67,7 +67,7 @@ public class MenuManager : MonoBehaviour
     /// </summary>
     private void RenderPlayerPortrait()
     {
-        playerPortrait.GetComponent<PortraitRenderer>().RenderPortrait(GameManager.Instance.Player.Color, OpenPortraitCustomizer, GameManager.Instance.Player.Portrait);
+        playerPortrait.GetComponent<PortraitRenderer>().RenderPortrait(GameManager.Instance.Player.Color, OpenPortraitCustomizer, GameManager.Instance.Player.Portrait, false);
     }
 
     /// <summary>
@@ -78,7 +78,7 @@ public class MenuManager : MonoBehaviour
         foreach (var commander in GameManager.Instance.Enemies)
         {
             GameObject instanciatedCommander = Instantiate(this.commanderPortraitPrefab, this.commandersGrid);
-            instanciatedCommander.GetComponent<PortraitRenderer>().RenderPortrait(commander.Color, () => StartBattle(commander), commander.Portrait);
+            instanciatedCommander.GetComponent<PortraitRenderer>().RenderPortrait(commander.Color, () => StartBattle(commander), commander.Portrait, commander.Locked);
         }
     }
 
--- Assets/Scripts/UI/PortraitRenderer.cs
+++ Assets/Scripts/UI/PortraitRenderer.cs
@@ -11,6 +11,8 @@ public class PortraitRenderer : MonoBehaviour
     [SerializeField]
     private Button btn;
     [SerializeField]
+    private GameObject face;
+    [SerializeField]
     private Image hair;
     [SerializeField]
     private Image eyes;
@@ -25,12 +27,27 @@ public class PortraitRenderer : MonoBehaviour
     /// <param name="hairSprite"></param>
     /// <param name="eyesSprite"></param>
     /// <param name="mouthSprite"></param>
-    public void RenderPortrait(Color commanderColor, UnityAction onClickAction, Portrait portrait)
+    public void RenderPortrait(Color commanderColor, UnityAction onClickAction, Portrait portrait, bool locked)
     {
-        background.color = commanderColor;
+        // Color portrait
+        if (!locked)
+        {
+            background.color = commanderColor;
+        }
+        else
+        {
+            background.color = new Color(.5f, .5f, .5f);
+        }
+
+        // Button
         btn.onClick.AddListener(onClickAction);
+        btn.interactable = !locked;
+
+        // Face attributes
         hair.sprite = portrait.Hair.sprite;
         eyes.sprite = portrait.Eye.sprite;
         mouth.sprite = portrait.Mouth.sprite;
+
+        face.SetActive(!locked);
     }
 }
