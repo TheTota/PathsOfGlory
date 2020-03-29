54737173886462a9215e9412b010a76dc38aae74
Thomas Cianfarani <thomas.ci83@gmail.com>
2020-03-29 22:01:32 +0200

    Add version displayer

---
+++ Assets/Scripts/UI/Version.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class Version : MonoBehaviour
+{
+    private void Awake()
+    {
+        this.GetComponent<TextMeshProUGUI>().text = Application.version;
+    }
+}
