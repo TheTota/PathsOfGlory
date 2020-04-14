5ac263061b3fbb5d0dd7df0ed85b12f8146d068e
Thomas Cianfarani <thomas.ci83@gmail.com>
2020-04-14 11:02:51 +0200

    Add units recap

---
+++ Assets/Scripts/UI/HelpUIManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpUIManager : MonoBehaviour
+{
+    public GameObject unitsRecapPanel;
+
+    public void SwitchUnitsRecapUIVisibility()
+    {
+        unitsRecapPanel.SetActive(!unitsRecapPanel.activeInHierarchy);
+    }
+
+}
