186a367e5851a5ee67b54ea163f307016f48f99c
Thomas Cianfarani <thomas.ci83@gmail.com>
2020-04-17 14:42:50 +0200

    Add battling units instanciation

--- Assets/UnitsFightManager.cs
+++ Assets/UnitsFightManager.cs
@@ -26,34 +26,62 @@ public class UnitsFightManager : MonoBehaviour
 
     public bool FightIsOver { get; internal set; }
 
+    private Camera mainCam;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.mainCam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void StartUnitsFight(UnitType playerUnit, UnitType enemyUnit)
     {
         this.FightIsOver = false;
 
+        // init vars
+        int unitsAmount = 6;
+        float yInc = this.maxBoundY - ((this.maxBoundY + this.minBoundY) / 2f);
+        float yIncMultiplicator = 0f;
+
+        float xSpacer = Random.Range(0.2f, 0.5f);
+
         // Instantiate player units
-        int unitsAmount = Random.Range(3, 5);
         for (int i = 0; i < unitsAmount; i++)
         {
-            // instantiate sprite
-            Vector3 instPos = new Vector3(-2f, Random.Range(this.minBoundY, this.maxBoundY), 1f);
-            GameObject instUnit = Instantiate(GetPrefabFromUnitType(playerUnit), instPos, Quaternion.identity, this.gameObject.transform);
-            // turn sprite around for the player
-            instUnit.transform.localScale = new Vector3(-instUnit.transform.localScale.x, instUnit.transform.localScale.y, instUnit.transform.localScale.z);
-            // color the sprite
-            instUnit.transform.Find("Body").GetComponent<SpriteRenderer>().color = this.bm.PlayerBC.Commander.Color;
+            if (i != 0 && i % 2 == 0) // si i est différent de 0 et paire, on incrémente la val pour la pos sur Y 
+            {
+                yIncMultiplicator++;
+                xSpacer = Random.Range(0.2f, 0.5f);
+            }
+
+            InstantiateUnit(this.bm.PlayerBC, playerUnit, yInc, yIncMultiplicator, -350f, xSpacer);
+            InstantiateUnit(this.bm.EnemyBC, enemyUnit, yInc, yIncMultiplicator, Screen.width + 200f, xSpacer);
+            xSpacer = Random.Range(1.5f, 1.75f);
         }
+    }
+
+    private void InstantiateUnit(BattleCommander bc, UnitType ut, float yInc, float yIncMultiplicator, float xOffset, float xSpacer)
+    {
+        // determine position
+        Vector3 instPos = new Vector3(this.mainCam.ScreenToWorldPoint(new Vector3(xOffset, 0f)).x + xSpacer, this.minBoundY + yInc * yIncMultiplicator, 1f);
+
+        // instantiate sprite
+        GameObject instUnit = Instantiate(GetPrefabFromUnitType(ut), instPos, Quaternion.identity, this.gameObject.transform);
+
+        // turn sprite for the player
+        if (bc == this.bm.PlayerBC)
+        {
+            instUnit.transform.localScale = new Vector3(-instUnit.transform.localScale.x, instUnit.transform.localScale.y, instUnit.transform.localScale.z);
+        }
+
+        // color the sprite
+        instUnit.transform.Find("Body").GetComponent<SpriteRenderer>().color = bc.Commander.Color;
     }
 
     private GameObject GetPrefabFromUnitType(UnitType ut)
